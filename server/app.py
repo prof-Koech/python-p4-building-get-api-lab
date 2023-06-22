88650f2d482c2303e3788fc26378f1dd6094cff5
Kennedy <kiplangatkennedy019@gmail.com>
2023-06-22 20:45:44 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -18,21 +18,77 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
-@app.route('/bakeries')
-def bakeries():
-    return ''
+@@app.route("/bakeries", methods=["GET"])
+def get_bakeries():
+    bakeries = Bakery.query.all()
+    bakery_list = []
+    for bakery in bakeries:
+        bakery_data = {
+            "id": bakery.id,
+            "name": bakery.name,
+            # Include other bakery attributes as needed
+        }
+        bakery_list.append(bakery_data)
+    return jsonify(bakery_list)
 
-@app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
 
-@app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
+from flask import Flask, jsonify, make_response
+from models import Bakery, BakedGood
 
-@app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+
+@app.route("/bakeries/<int:id>", methods=["GET"])
+def get_bakery(id):
+    bakery = Bakery.query.get(id)
+
+    if bakery is None:
+        response = {"error": "Bakery not found"}
+        return jsonify(response), 404
+
+    baked_goods = [baked_good.name for baked_good in bakery.baked_goods]
+
+    bakery_data = {
+        "id": bakery.id,
+        "name": bakery.name,
+        "baked_goods": baked_goods
+        # Include other bakery attributes as needed
+    }
+
+    return jsonify(bakery_data)
+
+
+@app.route("/baked_goods/by_price", methods=["GET"])
+def get_baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    baked_goods_data = []
+    for baked_good in baked_goods:
+        baked_good_data = {
+            "id": baked_good.id,
+            "name": baked_good.name,
+            "price": baked_good.price,
+            # Include other baked good attributes as needed
+        }
+        baked_goods_data.append(baked_good_data)
+
+    return jsonify(baked_goods_data)
+
+
+@app.route("/baked_goods/most_expensive", methods=["GET"])
+def get_most_expensive_baked_good():
+    most_expensive_baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    if most_expensive_baked_good is None:
+        # Handle the case when no baked goods are available
+        return jsonify({"message": "No baked goods found."}), 404
+
+    baked_good_data = {
+        "id": most_expensive_baked_good.id,
+        "name": most_expensive_baked_good.name,
+        "price": most_expensive_baked_good.price,
+        # Include other baked good attributes as needed
+    }
+
+    return jsonify(baked_good_data)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/testing/models_test.py
+++ server/testing/models_test.py
@@ -1,100 +1,28 @@
-from datetime import date
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
-from app import app
-from models import db, Bakery, BakedGood
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
-class TestBakery:
-    '''Bakery model in models.py'''
+db = SQLAlchemy(metadata=metadata)
 
-    with app.app_context():
-        b = Bakery.query.filter(Bakery.name == "Mr. Bakery")
-        for mb in b:
-            db.session.delete(mb)
-        db.session.commit()
 
-    def test_can_instantiate(self):
-        '''can be instantiated with a name.'''
-        b = Bakery(name="Mr. Bakery")
-        assert(b)
-    
-    def test_can_be_created(self):
-        '''can create records that can be committed to the database.'''
-        with app.app_context():
-            b = Bakery(name="Mr. Bakery")
-            db.session.add(b)
-            db.session.commit()
+class Bakery(db.Model, SerializerMixin):
+    __tablename__ = "bakeries"
 
-            db.session.delete(b)
-            db.session.commit()
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    baked_goods = db.relationship("BakedGood", backref="bakery", lazy=True)
 
-    def test_can_be_retrieved(self):
-        '''can be used to retrieve records from the database.'''
-        with app.app_context():
-            b = Bakery.query.all()
 
-    def test_can_be_serialized(self):
-        '''can create records with a to_dict() method for serialization.'''
-        with app.app_context():
-            b = Bakery(name="Mr. Bakery")
-            db.session.add(b)
-            db.session.commit()
-            bs = Bakery.query.first().to_dict()
-            assert(bs['id'])
-            assert(bs['created_at'])
-        
-            db.session.delete(b)
-            db.session.commit()
+class BakedGood(db.Model, SerializerMixin):
+    __tablename__ = "baked_goods"
 
-    def test_can_be_deleted(self):
-        '''can delete its records.'''
-        with app.app_context():
-            b = Bakery(name="Mr. Bakery")
-            db.session.add(b)
-            db.session.commit()
-
-            b = Bakery.query.filter(Bakery.name == "Mr. Bakery")
-            for mb in b:
-                db.session.delete(mb)
-            db.session.commit()
-
-class TestBakedGood:
-    '''BakedGood model in models.py'''
-
-    with app.app_context():
-        bg = BakedGood.query.filter(BakedGood.name == "Madeleine")
-        for m in bg:
-            db.session.delete(m)
-        db.session.commit()
-
-    def test_can_instantiate(self):
-        '''can be instantiated with a name and price.'''
-        bg = BakedGood(name="Madeleine", price=4)
-        assert(bg)
-    
-    def test_can_be_created(self):
-        '''can create records that can be committed to the database.'''
-        with app.app_context():
-            bg = BakedGood(name="Madeleine")
-            db.session.commit()
-
-    def test_can_be_retrieved(self):
-        '''can be used to retrieve records from the database.'''
-        with app.app_context():
-            bg = BakedGood.query.all()
-
-    def test_can_be_serialized(self):
-        '''can create records with a to_dict() method for serialization.'''
-        with app.app_context():
-            bg = BakedGood(name="Madeleine")
-            db.session.commit()
-            bgs = BakedGood.query.first().to_dict()
-            assert(bgs['id'])
-            assert(bgs['created_at'])
-
-    def test_can_be_deleted(self):
-        '''can delete its records.'''
-        with app.app_context():
-            bg = BakedGood.query.filter(BakedGood.name == "Madeleine")
-            for m in bg:
-                db.session.delete(m)
-            db.session.commit()
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer)
+    bakery_id = db.Column(db.Integer, db.ForeignKey("bakeries.id"), nullable=False)
